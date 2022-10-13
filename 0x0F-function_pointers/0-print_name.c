589eb14e1c33595c38995a194e17f439ef2171d8
Emani23paulo <Emani23paulo@gmail.com>
2022-10-12 17:15:19 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints namme through passed in function pointer
+ * print_name - prints name through passed in function pointer
  * @name: character string
  * @f: function pointer
  * Return: nothing
