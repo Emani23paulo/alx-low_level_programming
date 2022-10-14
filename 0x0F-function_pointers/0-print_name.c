02fd4019d0d61d2c985ea903d6522cdcab84d8a1
Emani23paulo <Emani23paulo@gmail.com>
2022-10-14 00:48:54 -0700

    Update 0-print_name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
-#include "function_pointers.h"
-
 /**
- * print_name - prints name through passed in function pointer
- * @name: character string
- * @f: function pointer
- * Return: nothing
+ *print_name - prints a name.
+ *@name: pointer to name.
+ *@f: function pointer.
+ *
+ *Return: void.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
+	if (name && f)
 		f(name);
 }
 
